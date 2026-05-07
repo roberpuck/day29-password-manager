3a8ad7e3011a16ef32d84e157f6ba16d56415b43
roberpuck <ecoceres@gmail.com>
2026-05-08 09:44:05 +1200

    Password manager completed using list comprehension

---
+++ main.py
@@ -0,0 +1,101 @@
+from random import shuffle
+from tkinter import *
+from tkinter import messagebox
+import random
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8,10)
+    nr_numbers = random.randint(2,4)
+    nr_symbols = random.randint(2,4)
+    password_letters = [random.choice(letters) for nr in range(nr_letters)]
+    password_numbers = [random.choice(numbers) for nr in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for nr in range(nr_symbols)]
+
+    password_generated = ''.join([item for sublist in [password_letters,password_numbers,password_symbols] for item in sublist])
+    pwd_entry.delete(0,END)
+    pwd_entry.insert(0,password_generated)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_password():
+    website = url.get()
+    mail = email.get()
+    password = pwd.get()
+    formatted_data = website +' | ' +mail+' | ' + password
+    formatted_data = f'{formatted_data}\n'
+
+    empty_verification = verify_emptyfields(website,mail,password)
+    print(empty_verification)
+
+    if empty_verification:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {mail}\nPassword: {password}\nIs it ok to save?")
+        if is_ok:
+            data_txt = open('./data.txt','a')
+            data_txt.writelines(formatted_data)
+            website_entry.delete(0,END)
+            email_entry.delete(0, END)
+            pwd_entry.delete(0, END)
+            website_entry.focus()
+            data_txt.close()
+        else:
+            website_entry.focus()
+    else:
+        website_entry.focus()
+
+def verify_emptyfields(website,mail,password):
+    data_to_verify = [website,mail,password]
+    for _ in data_to_verify:
+        if _ == '':
+            messagebox.showwarning(title='Oops',message="Please don't leave fields empty")
+            return False
+        else:
+            return True
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+pwd_window = Tk()
+pwd_window.configure(padx=50,pady=50)
+pwd_window.title('Password Manager')
+
+#Background
+bk_image = PhotoImage(file='./logo.png')
+canvas_pwd = Canvas(width=200,height=200)
+canvas_pwd.create_image(100,100,image=bk_image)
+canvas_pwd.grid(column=1,row=0)
+
+#Labels
+website_label = Label(text='Website:')
+email_label = Label(text='Email/Username:')
+password_label = Label(text='Password:')
+website_label.grid(column=0,row=1)
+email_label.grid(column=0,row=2)
+password_label.grid(column=0,row=3)
+
+#Input text Entries
+url = StringVar()
+pwd = StringVar()
+email = StringVar()
+website_entry = Entry(pwd_window,textvariable=url,font=('calibre',10),width=36,justify='left')
+email_entry = Entry(pwd_window,textvariable=email,font=('calibre',10),justify='left', width=36)
+pwd_entry = Entry(pwd_window,textvariable=pwd,font=('calibre',10),width=25)
+website_entry.grid(column=1,columnspan=2,row=1,sticky='w')
+website_entry.focus()
+email_entry.grid(column=1,columnspan=2,row=2,sticky='w')
+email_entry.insert(0,'example@domain.com')
+pwd_entry.grid(column=1,row=3,sticky='w')
+
+#Buttons
+generate_button = Button(text='Generate Password',font=('calibre',10),command=generate_password)
+add_button = Button(text='Add',font=('calibre',10),command=add_password,width=31,justify='left')
+generate_button.grid(column=2,row=3,sticky='w')
+add_button.grid(column=1,row=4,columnspan=2,sticky='w')
+
+
+pwd_window.mainloop()
